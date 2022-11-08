1425bdbc6a9744dd910b6d986df7ba0b84fca4bd
こうへい <k.sawada.sys22@morijyobi.ac.jp>
2022-11-08 16:03:43 +0900

    2人分入力、計算出来るように変更

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@ public class Main {
 public static void main(String[] args) {
 Scanner scanner = new Scanner(System.in);
 System.out.print("身長(cm)=");
+System.out.print("身長(cm)=");
 double height = scanner.nextDouble() / 100;
+System.out.print("体重(kg)=");
 System.out.print("体重(kg)=");
 double weight = scanner.nextDouble();
 double BMI= + weight / height / height;
